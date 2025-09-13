f80cbfef59b0c35709521dc5f833b2dcc6958d62
Dan Vasques <dan.vasques@outlook.com.br>
2025-09-13 16:22:19 -0300

    Criado aquivo bd.py para emular o banco de dados com os dados do IBGE. Ref #3

---
+++ backend/bd.py
@@ -0,0 +1,16 @@
+dados_agro = [
+    # Arroz
+    {"estado": "SP", "produto": "Arroz", "quantidade": 52138},
+    {"estado": "PR", "produto": "Arroz", "quantidade": 130880},
+    {"estado": "MG", "produto": "Arroz", "quantidade": 94693},
+
+    # Café
+    {"estado": "SP", "produto": "Café", "quantidade": 335310},
+    {"estado": "PR", "produto": "Café", "quantidade": 40206},
+    {"estado": "MG", "produto": "Café", "quantidade": 1687329},
+
+    # Feijão
+    {"estado": "SP", "produto": "Feijão", "quantidade": 199126},
+    {"estado": "PR", "produto": "Feijão", "quantidade": 860843},
+    {"estado": "MG", "produto": "Feijão", "quantidade": 531031}
+]
