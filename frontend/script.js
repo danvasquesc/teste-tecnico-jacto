9629c4249186d904463e73bea59fe28eb73eb9df
Dan Vasques <dan.vasques@outlook.com.br>
2025-09-14 14:35:35 -0300

    Integrado Chart.js e exibido gráfico de barras com quantidade por estado. Ref #5

--- frontend/script.js
+++ frontend/script.js
@@ -4,15 +4,19 @@ async function buscarDados() {
     try {
         const resposta = await fetch(apiUrl);
         const dados = await resposta.json();
-        console.log("Dados recebidos da API:", dados); //Teste
-        preecherTabela(dados);
+        console.log("Dados recebidos da API:", dados);
+
+        preencherTabela(dados);
+        criarGrafico(dados)
+
         return dados;
     } catch (error) {
         console.error("Erro ao buscar dados: ", error);
     }
 }
 
-function preecherTabela(dados) {
+// Tabela
+function preencherTabela(dados) {
     const tbody = document.querySelector("#tabela-dados tbody");
     tbody.innerHTML = "";
 
@@ -33,6 +37,81 @@ function preecherTabela(dados) {
 
         tbody.appendChild(tr);
     });
+}
+
+// Grafico
+function agruparDadosGrafico(dados) {
+    const quantidadePorEstado = {};
+
+    dados.forEach(item => {
+        const estado = item.estado;
+        const quantidade = item.quantidade;
+
+        if (quantidadePorEstado[estado] === undefined) {
+            quantidadePorEstado[estado] = quantidade;
+        } else {
+            quantidadePorEstado[estado] += quantidade;
+        }
+        
+    });
+
+    return quantidadePorEstado;
+}
+
+let graficoAtual = null;
+
+function criarGrafico(dados) {
+
+    const quantidadePorEstado = agruparDadosGrafico(dados);
+
+    const estados = Object.keys(quantidadePorEstado);
+    const valores = Object.values(quantidadePorEstado);
+
+    const ctx = document.getElementById("grafico-barras").getContext("2d");
+
+    if (graficoAtual) {
+        graficoAtual.destroy();
+    }
+
+    graficoAtual = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: estados, //
+        datasets: [{
+            label: 'Quantidade por Estado',
+            data: valores,
+
+            backgroundColor: [
+                'rgba(38, 255, 0, 1)',
+                'rgba(38, 255, 0, 1)',
+                'rgba(38, 255, 0, 1)'
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    callback: function(value) {
+                        return value.toLocaleString();
+                    }
+                }
+            }
+        },
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function(context) {
+                        const valor = context.parsed.y;
+                        return valor.toLocaleString();
+                    }
+                }
+            }
+        }
+    }
+});
 }
 
 buscarDados();
